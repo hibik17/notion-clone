623312754859201fa5f40ad972bfc1c05bf2c398
hibik17 <hbtatsu@gmail.com>
2022-12-31 18:57:38 +0900

    reset all vue codings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    {{ msg }}
+  </div>
 </template>
+
+<script setup>
+const msg = "hello vue";
+</script>
