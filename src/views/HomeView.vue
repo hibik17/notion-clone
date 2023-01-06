552f0c313a029aa0aee2a88fe98b3b91003d2ea6
hibik17 <hbtatsu@gmail.com>
2023-01-06 23:26:09 +0900

    contentの幅の作成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,10 @@
     <div class="my-[50px]">
       <hr />
     </div>
+    <div class="grid grid-cols-3">
+      <div class="w-full h-full col-span-2 bg-red-300">white</div>
+      <div class="w-full h-full bg-slate-300">ben white</div>
+    </div>
   </div>
 </template>
 
